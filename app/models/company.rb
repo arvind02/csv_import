1162723f18839e0007fd7b020745432dbc0cdb4b
arvind02 <er.arvind02@gmail.com>
2019-07-01 00:29:51 +0530

    Indentation

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,22 +5,22 @@ class Company < ApplicationRecord
   validates :name, presence: true
 
   def self.import_data(file_path, company_id)
-		import_boss_data(file_path, company_id)		  	  
+    import_boss_data(file_path, company_id)         
     import_employee_data(file_path, company_id)    
   end
 
   def self.import_boss_data(file_path, company_id)
-  	CSV.foreach(file_path, headers: true) do |row|
-			next if row.blank? || !row[3].nil?
-			process_data(row, '', company_id)
-		end
+    CSV.foreach(file_path, headers: true) do |row|
+      next if row.blank? || !row[3].nil?
+      process_data(row, '', company_id)
+    end
   end
 
   def self.import_employee_data(file_path, company_id)
-  	boss_hash = Employee.boss_employees_data
-  	CSV.foreach(file_path, headers: true) do |row|
-     	next if row.blank? || row[3].nil?
- 			parent_id = boss_hash[row[3]] rescue ''
+    boss_hash = Employee.boss_employees_data
+    CSV.foreach(file_path, headers: true) do |row|
+      next if row.blank? || row[3].nil?
+      parent_id = boss_hash[row[3]] rescue ''
       process_data(row, parent_id, company_id)
     end 
   end
@@ -28,13 +28,13 @@ class Company < ApplicationRecord
   private 
 
   def self.process_data(row, parent_id, company_id)
-  	employee = Employee.add_or_get_employees(row,parent_id,company_id)
-		policy_data = Policy.add_or_get_policies(row, company_id)
-		if !policy_data.blank? && !employee.blank?
-			policy_data.each do |policy|
-				employee.policies << policy
-			end	
-		end
+    employee = Employee.add_or_get_employees(row,parent_id,company_id)
+    policy_data = Policy.add_or_get_policies(row, company_id)
+    if !policy_data.blank? && !employee.blank?
+      policy_data.each do |policy|
+        employee.policies << policy
+      end 
+    end
   end
 
 end
